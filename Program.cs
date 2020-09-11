4e06b20e80c9b7571a3c5088576e0b8b01c40482
Hugh McGee <hugh41971@gmail.com>
2020-09-11 14:25:53 -0400

    number one completed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace ComputationalThinkingExercises
 {
@@ -27,7 +28,7 @@ namespace ComputationalThinkingExercises
 
                 string userChoice = Console.ReadLine().ToLower();
 
-                switch(userChoice)
+                switch (userChoice)
                 {
                     case "1":
                         AreTheseIntegersEqual();
@@ -82,176 +83,189 @@ namespace ComputationalThinkingExercises
             Console.Clear();
             Console.WriteLine("ARE THESE INTEGERS EQUAL?\n");
 
-            // Enter your solution here
-
+            Console.WriteLine("I'm looking to compare two numbers. May I have a number,please?");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Cool. May I have another?");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            if (firstNumber == secondNumber)
+            {
+                Console.WriteLine("Your numbers...MATCH! Tonight, we feast!!!");
+            }
+            else
+            {
+                Console.WriteLine("Your numbers are not equal. This ...disappoints me.");
+            }
             Console.Write("Press enter to return to the Main Menu");
             Console.ReadLine();
         }
 
-        static void EvenOrOdd()
-        {
-            // Write a console application that asks the user for a number.
-            // Tell the user if that number is even or odd.
 
-            Console.Clear();
-            Console.WriteLine("EVEN OR ODD?\n");
 
-            // Enter your solution here
 
-            Console.Write("Press enter to return to the Main Menu");
-            Console.ReadLine();
+            static void EvenOrOdd()
+            {
+                // Write a console application that asks the user for a number.
+                // Tell the user if that number is even or odd.
+
+                Console.Clear();
+                Console.WriteLine("EVEN OR ODD?\n");
+
+                // Enter your solution here
+
+                Console.Write("Press enter to return to the Main Menu");
+                Console.ReadLine();
+            }
+
+            static void VowelOrConsonant()
+            {
+                // Write a console application to check whether a letter is a vowel or consonant.
+                // Hint: You can do this problem using a switch or an if condition.
+
+                Console.Clear();
+                Console.WriteLine("VOWEL OR CONSONANT?\n");
+
+                // Enter your solution here
+
+                Console.Write("Press enter to return to the Main Menu");
+                Console.ReadLine();
+            }
+
+            static void FindLargestNumber()
+            {
+                // Write a console application that prompts the user to input two integer values.
+                // Find and print the greatest value of the two integers.
+
+                Console.Clear();
+                Console.WriteLine("FIND THE LARGEST NUMBER\n");
+
+                // Enter your solution here
+
+                Console.Write("Press enter to return to the Main Menu");
+                Console.ReadLine();
+            }
+
+            static void DivisibleBy3()
+            {
+                // Ask the user for a number and then tell them if it is divisible by 3
+
+                Console.Clear();
+                Console.WriteLine("DIVISIBLE BY 3\n");
+
+                // Enter your solution here
+
+                Console.Write("Press enter to return to the Main Menu");
+                Console.ReadLine();
+            }
+
+            static void RestaurantBill()
+            {
+                // At a restaurant, Mike and his three friends decided to divide the bill evenly.
+                // If each person paid $13 then what was the total bill?
+
+                // Returning to the above problem, replace hard-coded values of 4(number of diners)
+                // and 13(cost per diner) with values provided by the user.
+
+                // How nice!The restaurant is having Customer Appreciation Week.
+                // If a table’s total bill is $50 or more, then they’ll receive a 10 % discount!
+                // Otherwise, they’ll receive a 5 % discount.
+
+                Console.Clear();
+                Console.WriteLine("RESTAURANT BILL\n");
+
+                // Enter your solution here
+
+                Console.Write("Press enter to return to the Main Menu");
+                Console.ReadLine();
+            }
+
+            static void AgeCategory()
+            {
+                // You're responsible for providing a demographic report for your local school district based on age.
+                // To do this, you're going to determine which 'category' each person fits into based on their age.
+                // The person's age will determine which category they should be in:
+
+                // If they're from 0 to 2 the child should be with parents print : 'Still in Mama's Arms'
+                // If they're 3 or 4 and should be in preschool print : 'Preschool Maniac'
+                // If they're from 5 to 11 and should be in elementary school print : 'Elementary School'
+                // From 12 to 14: 'Middle School'
+                // From 15 to 18: 'High School'
+                // From 19 to 22: 'College'
+                // From 23 to 65: 'Working for the Man'
+                // From 66 to 100: 'The Golden Years'
+                // If the age of the person is less than 0 or more than 100 - it might be an alien
+                // print: "This program is for humans".
+
+                Console.Clear();
+                Console.WriteLine("AGE CATEGORY\n");
+
+                // Enter your solution here
+
+                Console.Write("Press enter to return to the Main Menu");
+                Console.ReadLine();
+            }
+
+            static void WordsToDigits()
+            {
+                // Having a string representation of a number, you need to print the digit form of the number.
+                // Ask the user to enter a string representation of a number from zero to ten.
+                // Using switch case, print the digit (0-10) representation of the number.
+
+                Console.Clear();
+                Console.WriteLine("WORDS TO DIGITS\n");
+
+                // Enter your solution here
+
+                Console.Write("Press enter to return to the Main Menu");
+                Console.ReadLine();
+            }
+
+            static void WhichNameIsLonger()
+            {
+                // Ask the user for their first and last name.
+
+                // Print the user's full name.
+                // If the first name is longer than the last name, print "First is longer."
+                // If the first name and last name are the same length, print "Same-sies!"
+                // Otherwise, print "Last must be longer!"
+
+                Console.Clear();
+                Console.WriteLine("WHICH NAME IS LONGER?\n");
+
+                // Enter your solution here
+
+                Console.Write("Press enter to return to the Main Menu");
+                Console.ReadLine();
+            }
+
+            static void AreNamesSame()
+            {
+                // Ask the user for 2 names.
+
+                // If the two names are the same, print "The names are the same."
+                // If they're not the same, print "The names are different."
+
+                Console.Clear();
+                Console.WriteLine("ARE THESE NAMES THE SAME?\n");
+
+                // Enter your solution here
+
+                Console.Write("Press enter to return to the Main Menu");
+                Console.ReadLine();
+            }
+
+            static void NameAndBirthplace()
+            {
+                // Ask the user for their first name and where they were born.
+                // Print a sentence to the console that repeats this information.
+
+                Console.Clear();
+                Console.WriteLine("NAME AND PLACE OF BIRTH\n");
+
+                // Enter your solution here
+
+                Console.Write("Press enter to return to the Main Menu");
+                Console.ReadLine();
+            }
+
         }
-
-        static void VowelOrConsonant()
-        {
-            // Write a console application to check whether a letter is a vowel or consonant.
-            // Hint: You can do this problem using a switch or an if condition.
-
-            Console.Clear();
-            Console.WriteLine("VOWEL OR CONSONANT?\n");
-
-            // Enter your solution here
-
-            Console.Write("Press enter to return to the Main Menu");
-            Console.ReadLine();
-        }
-
-        static void FindLargestNumber()
-        {
-            // Write a console application that prompts the user to input two integer values.
-            // Find and print the greatest value of the two integers.
-
-            Console.Clear();
-            Console.WriteLine("FIND THE LARGEST NUMBER\n");
-
-            // Enter your solution here
-
-            Console.Write("Press enter to return to the Main Menu");
-            Console.ReadLine();
-        }
-
-        static void DivisibleBy3()
-        {
-            // Ask the user for a number and then tell them if it is divisible by 3
-
-            Console.Clear();
-            Console.WriteLine("DIVISIBLE BY 3\n");
-
-            // Enter your solution here
-
-            Console.Write("Press enter to return to the Main Menu");
-            Console.ReadLine();
-        }
-
-        static void RestaurantBill()
-        {
-            // At a restaurant, Mike and his three friends decided to divide the bill evenly.
-            // If each person paid $13 then what was the total bill?
-
-            // Returning to the above problem, replace hard-coded values of 4(number of diners)
-            // and 13(cost per diner) with values provided by the user.
-            
-            // How nice!The restaurant is having Customer Appreciation Week.
-            // If a table’s total bill is $50 or more, then they’ll receive a 10 % discount!
-            // Otherwise, they’ll receive a 5 % discount.
-
-            Console.Clear();
-            Console.WriteLine("RESTAURANT BILL\n");
-
-            // Enter your solution here
-
-            Console.Write("Press enter to return to the Main Menu");
-            Console.ReadLine();
-        }
-
-        static void AgeCategory()
-        {
-            // You're responsible for providing a demographic report for your local school district based on age.
-            // To do this, you're going to determine which 'category' each person fits into based on their age.
-            // The person's age will determine which category they should be in:
-
-            // If they're from 0 to 2 the child should be with parents print : 'Still in Mama's Arms'
-            // If they're 3 or 4 and should be in preschool print : 'Preschool Maniac'
-            // If they're from 5 to 11 and should be in elementary school print : 'Elementary School'
-            // From 12 to 14: 'Middle School'
-            // From 15 to 18: 'High School'
-            // From 19 to 22: 'College'
-            // From 23 to 65: 'Working for the Man'
-            // From 66 to 100: 'The Golden Years'
-            // If the age of the person is less than 0 or more than 100 - it might be an alien
-                    // print: "This program is for humans".
-
-            Console.Clear();
-            Console.WriteLine("AGE CATEGORY\n");
-
-            // Enter your solution here
-
-            Console.Write("Press enter to return to the Main Menu");
-            Console.ReadLine();
-        }
-
-        static void WordsToDigits()
-        {
-            // Having a string representation of a number, you need to print the digit form of the number.
-            // Ask the user to enter a string representation of a number from zero to ten.
-            // Using switch case, print the digit (0-10) representation of the number.
-
-            Console.Clear();
-            Console.WriteLine("WORDS TO DIGITS\n");
-
-            // Enter your solution here
-
-            Console.Write("Press enter to return to the Main Menu");
-            Console.ReadLine();
-        }
-
-        static void WhichNameIsLonger()
-        {
-            // Ask the user for their first and last name.
-
-            // Print the user's full name.
-            // If the first name is longer than the last name, print "First is longer."
-            // If the first name and last name are the same length, print "Same-sies!"
-            // Otherwise, print "Last must be longer!"
-
-            Console.Clear();
-            Console.WriteLine("WHICH NAME IS LONGER?\n");
-
-            // Enter your solution here
-
-            Console.Write("Press enter to return to the Main Menu");
-            Console.ReadLine();
-        }
-
-        static void AreNamesSame()
-        {
-            // Ask the user for 2 names.
-
-            // If the two names are the same, print "The names are the same."
-            // If they're not the same, print "The names are different."
-
-            Console.Clear();
-            Console.WriteLine("ARE THESE NAMES THE SAME?\n");
-
-            // Enter your solution here
-
-            Console.Write("Press enter to return to the Main Menu");
-            Console.ReadLine();
-        }
-
-        static void NameAndBirthplace()
-        {
-            // Ask the user for their first name and where they were born.
-            // Print a sentence to the console that repeats this information.
-
-            Console.Clear();
-            Console.WriteLine("NAME AND PLACE OF BIRTH\n");
-
-            // Enter your solution here
-
-            Console.Write("Press enter to return to the Main Menu");
-            Console.ReadLine();
-        }
-
     }
-}
